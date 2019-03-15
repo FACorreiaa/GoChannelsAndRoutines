5087a75da5b083fba9f320048ee4bdea575af9b7
Fernando <a11199@alunos.ipca.pt>
2019-03-15 14:13:17 +0000

    check site status with channels and routines

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,8 +21,20 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	/*for i := 0; i < len(links); i++ {
+		//fmt.Println(<-c)
+
+		go checkLink(link, <-c)
+	}*/
+	/*for {
+		go checkLink(<-c, c)
+	}*/
+	//or
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 
 }
@@ -30,10 +43,10 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down")
-		c <- "might be down"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Its up"
+	c <- link
 }
